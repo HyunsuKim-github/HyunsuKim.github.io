069688b63103ea110720929adcbb288f50561bc8
Kim Hyunsu <kimhyunsu@gmail.com>
2024-08-19 17:45:16 +0900

    Updates

---
+++ precache-manifest.1e6566a48ceee7c963b8e2cf7d5d8137.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d36ed8664ce2dec601801bd8bb293e2b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8b428d775826e6ba3908",
+    "url": "/static/js/2.019b2db8.chunk.js"
+  },
+  {
+    "revision": "ee263cc156f1885d538a",
+    "url": "/static/js/main.0877a5ca.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
